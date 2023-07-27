d941b587ca65d87b99c2dfcf8cb99da2b18438a0
Joshua Ho Gwok Hin <hgh.joshua@gmail.com>
2023-07-27 17:40:31 +0800

    Create

---
+++ MethodReferencing/src/MyInterfaces.java
@@ -0,0 +1,7 @@
+public interface MyInterfaces {}
+interface VoidEmpty {void voidEmpty();}
+interface VoidString {void voidString(String s);}
+interface IntEmpty {int intEmpty();}
+interface IntString {int intString(String s);}
+interface IntBooleanString{ int intBooleanString(boolean b, String s);}
+interface IntUser {int intUser(User u);}
--- MethodReferencing/src/User.java
+++ MethodReferencing/src/User.java
@@ -1,2 +1,7 @@
 public class User {
+    void voidEmpty() {}
+    void voidString(String s) {}
+    int intEmpty(){ return 1;}
+    int intString(String s) { return 2;}
+    int intBooleanString(boolean b, String s) { return 3;}
 }
