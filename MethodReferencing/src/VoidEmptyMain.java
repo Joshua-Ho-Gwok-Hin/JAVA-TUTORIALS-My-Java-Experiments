64b2b4ef543233d62f0af554edfef31d50c51457
Joshua Ho Gwok Hin <hgh.joshua@gmail.com>
2023-07-27 21:14:43 +0800

    Create

---
+++ MethodReferencing/src/VoidEmptyMain.java
@@ -0,0 +1,62 @@
+public class VoidEmptyMain {
+    public static void main(String[] args) {
+        User john = new User();
+        VoidEmpty voidEmptyFI;
+        String str = "Hello World";
+        int myInt = 2;
+
+        voidEmptyFI = ()->{};
+        new VoidEmpty() {
+            @Override
+            public void voidEmpty() {}
+        };
+
+        voidEmptyFI = System.out::println;
+        voidEmptyFI = () -> System.out.println();
+        new VoidEmpty() {
+            @Override
+            public void voidEmpty() {
+                System.out.println();
+            }
+        };
+
+        voidEmptyFI = john::voidEmpty;
+        voidEmptyFI = () -> john.voidEmpty();
+        new VoidEmpty(){
+            public void voidEmpty() {
+                john.voidEmpty();
+            }
+        };
+
+        voidEmptyFI = () -> john.voidString(str);
+        new VoidEmpty() {
+            public void voidEmpty() {
+                john.voidString(str);
+            }
+        };
+
+
+        voidEmptyFI = john::intEmpty;
+        voidEmptyFI = () -> john.intEmpty();
+        new VoidEmpty() {
+            @Override
+            public void voidEmpty() {
+                john.intEmpty();
+            }
+        };
+
+        voidEmptyFI = () -> john.intString(str);
+        new VoidEmpty() {
+            public void voidEmpty() {
+                john.intString(str);
+            }
+        };
+
+        voidEmptyFI = () -> john.intBooleanString(true, str);
+        new VoidEmpty(){
+            public void voidEmpty() {
+                john.intBooleanString(true, str);
+            }
+        };
+    }
+}
