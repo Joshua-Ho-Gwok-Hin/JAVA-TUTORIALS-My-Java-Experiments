d72e2d7de3e68a56f1dd97a1bde65cddafe01f07
Joshua Ho Gwok Hin <hgh.joshua@gmail.com>
2023-07-28 01:47:08 +0800

    Create

--- MethodReferencing/src/VoidStringMain.java
+++ MethodReferencing/src/VoidStringMain.java
@@ -1,2 +1,36 @@
 public class VoidStringMain {
+    public static void main(String[] args) {
+        User john = new User();
+
+        VoidString voidStringFI;
+        voidStringFI = System.out::println;
+        voidStringFI = s -> System.out.println(s);
+        new VoidString(){
+            @Override
+            public void voidString(String s) {
+                System.out.println(s);
+            }
+        };
+
+        voidStringFI = john::voidString;
+        voidStringFI = s -> john.voidString(s);
+        new VoidString(){
+            @Override
+            public void voidString(String s) {
+                john.voidString(s);
+            }
+        };
+
+        voidStringFI = john::intString;
+        voidStringFI = s -> john.intString(s);
+        new VoidString(){
+            public void voidString(String s) {
+                john.intString(s);
+            }
+        };
+
+        voidStringFI = john::voidEmpty;
+        voidStringFI = john::intEmpty;
+        voidStringFI = john::intBooleanString;
+    }
 }
